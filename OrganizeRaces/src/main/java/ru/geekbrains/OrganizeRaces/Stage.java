86a7f5b8d98fbca356a8b66ebf6a8afd112e0130
gendalf1366 <67900577+gendalf1366@users.noreply.github.com>
2021-01-19 20:17:24 +0300

    lesson5

---
+++ OrganizeRaces/src/main/java/ru/geekbrains/OrganizeRaces/Stage.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.OrganizeRaces;
+
+public abstract class Stage {
+    int length;
+    String description;
+
+    public String getDescription() {
+
+        return description;
+    }
+
+    public abstract void go(Car c);
+}
